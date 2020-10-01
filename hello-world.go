bb675d3501a98c0baa50cc73c3ef8fa448927e92
Anjo Tadena <tadena.aa@sprobe.com>
2020-10-02 07:31:16 +0800

    Create awesome hello world program

---
+++ hello-world.go
@@ -0,0 +1,18 @@
+// # Running go file
+// $ go run hello-world.go
+// > Hello, World!
+
+// $ go build hello-world.go
+// $ ls
+// > hello-world hello-world.go
+
+// $ ./hello-world
+// Hello, World!
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
